e0ae3a03223d155b50a3ca77772c20fa35193379
NebbyTC <mr.nikodemis@gmail.com>
2026-09-16 12:14:32 +0200

    feat: switch the program to auditid logs

--- auth_inspector.py
+++ auth_inspector.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
-import logging
 import os
-import subprocess
 import sys
-import time
-import re
+import logging
 
 try:
 	from systemd import journal 
@@ -16,86 +13,14 @@ LOG_DIR = "/var/log/auth-inspector"
 LOG_FILE = LOG_DIR + "/incidents.log"
 
 
-class TerminalSession:
+def is_root() -> bool:
 	"""
-		Represents a terminal, from which someone tried to authenticate.
+		Checks if the program is running as root.
 	"""
 
-	def __init__(self, fail_type: str, tty: str, ip_address: str | None):
-		"""
-			Assigns key session attributes to the object.
-		"""
-		
-		self.fail_type = fail_type.upper()
-		self.tty = tty
-		self.ip_address = ip_address
-		
-	def is_local(self) -> bool:
-		"""
-			Determines whether the terminal session represented by the object 
-			is local.
-		"""
-		return not bool(self.ip_address)
-
-	@classmethod
-	def from_log_line(cls, message: str, fail_type: str):
-		"""
-			Creates a functioning session object.
-		"""
-		
-		tty = cls._extract_tty(message)
-		ip_address = cls._fetch_ip_from_system(tty)
-
-		return cls(fail_type=fail_type, tty=tty, ip_address=ip_address)
-
-	@staticmethod
-	def _extract_tty(log_line: str) -> str:
-		"""
-			Returns the id of the terminal with the failed authentication try.
-		
-			Raises:
-				String -> TTY id extracted from the given log line.
-			
-				RuntimeError -> if any information about TTY tag inside 
-				the given entry was not found.
-		"""
-		
-		match = re.search(r'tty=(?:/dev/)?([^\s;]+)', log_line)
-		if not match: 
-			raise LookupError("[Auth-inspector] Couldn't find TTY information.")
-		
-		return match.group(1)
-
-	@staticmethod
-	def _fetch_ip_from_system(tty_name: str) -> str | None:
-		"""
-			Returns an IP addres tied to the given tty terminal, if exists.
-		
-			Returns:
-				str -> IP address tied to the given TTY id.
-			
-				None -> if the given terminal session turns out to be a local one.
-		
-			Raises:
-				RuntimeError -> if a terminal with given tty id was not found.
-		"""
-		
-		result = subprocess.run(['who'], capture_output=True, text=True, check=True)
-		
-		for terminal_data in result.stdout.splitlines():
-			incorrect_terminal = not tty_name in terminal_data
-			if incorrect_terminal: continue
-		
-			data_splitted = terminal_data.split()
-		
-			is_not_ssh = len(data_splitted) < 5
-			if is_not_ssh:
-				return None
-		
-			return data_splitted[5].strip("()")
-
-		raise LookupError(f"[Auth-inspector] Terminal {tty_name} was not found in active sessions.")
-
+	if os.geteuid() != 0:
+		return False
+	return True
 
 def on_startup() -> None:
 	"""
@@ -103,7 +28,7 @@ def on_startup() -> None:
 	"""
 
 	# Ensuring that the script runs as root
-	if os.geteuid() != 0:
+	if not is_root():
 		print("[Auth-inspector] Error: Please run the script as root.", file=sys.stderr)
 		sys.exit(1)
 
@@ -117,58 +42,35 @@ def on_startup() -> None:
 		handlers=[logging.StreamHandler(sys.stdout)]
 	)
 
-
-def log_incident(session: TerminalSession) -> None:
-	"""
-		Logs the incident to the auth-inspector log file.
-	"""
-
-	timestamp = time.strftime("%b %d %H:%M:%S")
-	log_line = f"{timestamp} auth-inspector: {session.fail_type} auth failed from IP={session.ip_address} on {session.tty}\n"
-	
-	with open(LOG_FILE, "a", encoding="utf-8") as f:
-		f.write(log_line)
-
-	logging.info(f"[Auth inspector] Failed auth attempt on {session.fail_type}: TTY={session.tty}, IP={session.ip_address}")
-
-
 def run() -> None:
-	"""
-		The main loop that listens to the systemd core events.
-	"""
 
-	if journal is None:
-		logging.warning("[Auth inspector] OS doesn't support systemd. Listening mode activated (test simulation).")
-		return
-	
 	try:
 		reader = journal.Reader()
-		reader.this_boot()
-		reader.add_match(_COMM="sudo")
-		reader.add_match(_COMM="su")
 		reader.seek_tail()
 		reader.get_previous()
+
+		reader.add_match(SYSLOG_IDENTIFIER="audisp-syslog")
+
 	except OSError as err:
-		logging.critical("[Auth inspector] Cannot reach systemd logs: %s", err)
+		print(f"Error: Could not acces Systemd Journal: {err}", file=sys.stderr)
+		sys.exit(1)
+
+	except NameError:
+		print("Error: Dependency systemd.journal not found. Please run: apt install python3-systemd", file=sys.stderr)
 		sys.exit(1)
 
 	logging.info("[Auth inspector] Start successful. Listening for authentication attempts...")
+	interesujace_typy = ["type=CRED_ACQ", "type=LOGIN", "type=USER_AUTH", "type=CRED_DISP", "type=USER_END"]
 
 	while True:
 		if reader.wait() == journal.NOP:
 			continue
 		
 		for entry in reader:
-			comm = entry.get('_COMM', '')
 			message = entry.get('MESSAGE', '')
-			if not "authentication failure" in message: continue
 
-			session = TerminalSession.from_log_line(message, comm)
-			if session.is_local():
-				continue
-
-			log_incident(session)
-
+			if any(typ in message for typ in interesujace_typy):
+				print(message.strip())
 
 if __name__ == "__main__":
 	on_startup()
