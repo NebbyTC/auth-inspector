b908b1366ddefb95a8b52dc859546dd6b2a983f7
NebbyTC <mr.nikodemis@gmail.com>
2026-09-16 18:06:08 +0200

    test: remove the final test

--- test_auth_inspector_integration.py
+++ test_auth_inspector_integration.py
@@ -137,35 +137,3 @@ class TestTerminalSessionIntegration(unittest.TestCase):
 		
 		self.assertIn("/usr/bin/sudo", file_content, "Error: The program does not save the information about failed auth type properly.")
 		self.assertIn("192.168.1.67", file_content, "Error: The Program does not save the information about the IP properly.")
-
-	def test_context_manager_wakes_up_correctly_on_live_event(self):
-		"""
-			Tests that the audit_journal_context opens successfully, 
-			and reader.wait() correctly handles a live systemd message 
-			injected via journal.send().
-		"""
-
-		inspector_thread = threading.Thread(target=run, daemon=True)
-		inspector_thread.start()
-		
-		time.sleep(1.0)
-
-		journal.send(
-			MESSAGE='type=CRED_ACQ msg=audit(1789554012.232:957): pid=17451 uid=0 auid=1000 ses=88 acct="prezes" exe="/usr/lib/openssh/sshd-session" addr=192.168.1.55 AUID="prezes"',
-			SYSLOG_IDENTIFIER='audisp-syslog'
-		)
-		time.sleep(0.1)
-		journal.send(
-			MESSAGE='type=USER_AUTH msg=audit(1789554015.555:958): pid=17460 uid=0 auid=1000 ses=88 acct="root" exe="/usr/bin/sudo" res=failed',
-			SYSLOG_IDENTIFIER='audisp-syslog'
-		)
-
-		time.sleep(5)
-		
-		self.assertTrue(os.path.exists(LOG_FILE), "Error: Log file was not created!")
-		
-		with open(LOG_FILE, "r") as f:
-			log_content = f.read()
-
-		self.assertIn("auth-inspector: Failed /usr/bin/sudo auth attempt from IP=192.168.1.55", log_content)
-
